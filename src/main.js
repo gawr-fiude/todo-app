f9b20adca57242195338e53eaf378ec927313614
gawr-fiude <eduardo.e.goto@gmail.com>
2024-11-09 18:14:01 -0300

    initial commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import "@catppuccin/palette/css/catppuccin.css";
+import "./assets/reset.css";
+import "./assets/main.css";
+
+import App from "./App.vue";
+import router from "./router";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
