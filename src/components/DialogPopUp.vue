856f90c0ab35a81f872af79c9c2648e3885a6ebc
gawr-fiude <eduardo.e.goto@gmail.com>
2024-11-17 14:34:49 -0300

    chore: create Dialog component to manage show/close modal

---
+++ src/components/DialogPopUp.vue
@@ -0,0 +1,62 @@
+<template>
+  <dialog
+    ref="dialog"
+    @keydown.esc="
+      () => {
+        $emit('close-modal');
+      }
+    "
+    @click="closeOnClickOutside"
+    class="padding--vertical padding--horizontal"
+  >
+    <slot></slot>
+  </dialog>
+</template>
+
+<script setup>
+import { useTemplateRef, watch } from "vue";
+
+const props = defineProps({
+  show: Boolean,
+});
+const emit = defineEmits(["close-modal"]);
+
+const dialog = useTemplateRef("dialog");
+
+watch(props, (newValue, _) => {
+  if (newValue.show === true) {
+    dialog.value.showModal();
+  } else {
+    dialog.value.close();
+  }
+});
+
+// The Dialog should close on clicks outside its main content
+// This function makes sure the Dialog is closed, even if the backdrop is clicked
+// VueUse's onClickOutside wouldn't work well since the backdrop is still 'inside'
+function closeOnClickOutside(event) {
+  let rect = dialog.value.getBoundingClientRect();
+
+  let isInDialog =
+    rect.top <= event.clientY &&
+    event.clientY <= rect.top + rect.height &&
+    rect.left <= event.clientX &&
+    event.clientX <= rect.left + rect.width;
+
+  if (!isInDialog) {
+    emit("close-modal");
+  }
+}
+</script>
+
+<style scoped>
+dialog {
+  color: var(--color-text);
+  background: var(--color-base);
+}
+
+dialog::backdrop {
+  background: var(--color-base);
+  opacity: 0.8;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,26 @@
 <template>
-  <button
-    @click="
-      () => {
-        dialogNewTodo.showModal();
-      }
-    "
-    class="focusable"
-  >
-    Add TODO
-  </button>
-  <button
-    @click="
-      () => {
-        dialogRemoveAll.showModal();
-      }
-    "
-    class="focusable"
-  >
-    Remove All
-  </button>
+  <div class="button-group">
+    <button
+      @click="
+        () => {
+          showAddTodoDialog = true;
+        }
+      "
+      class="focusable"
+    >
+      Add TODO
+    </button>
+    <button
+      @click="
+        () => {
+          showRemoveAllDialog = true;
+        }
+      "
+      class="focusable"
+    >
+      Remove All
+    </button>
+  </div>
 
   <ul>
     <li v-for="todo in todoStore.todos" :key="todo.createdDate + todo.label">
@@ -26,32 +28,49 @@
     </li>
   </ul>
 
-  <dialog ref="dialog-new-todo" class="padding--vertical padding--horizontal">
-    <form @submit.prevent="addTodo">
-      <fieldset>
-        <legend>Label</legend>
+  <DialogPopUp
+    :show="showAddTodoDialog"
+    @close-modal="
+      () => {
+        showAddTodoDialog = false;
+      }
+    "
+  >
+    <form @submit.prevent="addTodo" class="dialog-content">
+      <fieldset class="wrapper">
+        <legend class="legend">Label</legend>
         <input v-model="newTodo.label" type="text" />
       </fieldset>
 
-      <fieldset>
-        <legend>Description</legend>
+      <fieldset class="wrapper">
+        <legend class="legend">Description</legend>
         <input v-model="newTodo.description" type="text" />
       </fieldset>
 
       <button type="submit" class="focusable">Add</button>
     </form>
-  </dialog>
+  </DialogPopUp>
 
-  <dialog ref="dialog-remove-all">
-    <p>Delete all incomplete TODOs?</p>
+  <DialogPopUp
+    :show="showRemoveAllDialog"
+    @close-modal="
+      () => {
+        showRemoveAllDialog = false;
+      }
+    "
+  >
+    <article class="dialog-content">
+      <p>Delete all incomplete TODOs?</p>
 
-    <button>Cancel</button>
-    <button>Confirm</button>
-  </dialog>
+      <button class="focusable">Confirm</button>
+    </article>
+  </DialogPopUp>
 </template>
 
 <script setup>
-import { ref, useTemplateRef } from "vue";
+import { ref } from "vue";
+
+import DialogPopUp from "@/components/DialogPopUp.vue";
 
 import { useTodoStore } from "@/stores/todos.js";
 const todoStore = useTodoStore();
@@ -61,7 +80,8 @@ const newTodo = ref({
   description: "",
 });
 
-const dialogNewTodo = useTemplateRef("dialog-new-todo");
+const showAddTodoDialog = ref(false);
+const showRemoveAllDialog = ref(false);
 
 function addTodo() {
   todoStore.addTodo({
@@ -75,6 +95,8 @@ function addTodo() {
     name: "",
     description: "",
   };
+
+  showAddTodoDialog.value = false;
 }
 </script>
 
@@ -84,6 +106,28 @@ button {
 
   background: var(--color-text);
   color: var(--color-base);
-  margin-right: 1rem;
+}
+
+.button-group {
+  display: flex;
+  gap: 1rem;
+}
+
+.dialog-content {
+  display: grid;
+  gap: 2rem;
+}
+
+.dialog-content:has(input) {
+  width: 60ch;
+}
+
+input {
+  background: var(--color-base);
+  padding: var(--spacing-small);
+}
+
+input:focus-visible {
+  outline: 0;
 }
 </style>
